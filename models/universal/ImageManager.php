fbb36acc36be03c9f0d45ff0860889b85d02c131
a <a>
2025-02-20 09:28:15 +0100

    a

--- models/universal/ImageManager.php
+++ models/universal/ImageManager.php
@@ -51,6 +51,37 @@ abstract class ImageManager extends ApiResponse
 
     // MARK: FILE OPERATIONS
 
+    protected function flattenFilesArray(): array
+    {
+        $files = $_FILES;
+        $files2 = [];
+
+        foreach ($files as $input => $infoArr) {
+            $filesByInput = [];
+
+            foreach ($infoArr as $key => $valueArr) {
+                if (is_array($valueArr)) { // file input "multiple"
+                    foreach ($valueArr as $i => $value) {
+                        $filesByInput[$i][$key] = $value;
+                    }
+                } else { // -> string, normal file input
+                    $filesByInput[] = $infoArr;
+                    break;
+                }
+            }
+
+            $files2 = array_merge($files2, $filesByInput);
+        }
+
+        $files3 = [];
+
+        foreach ($files2 as $file) { // let's filter empty & errors
+            if (!$file['error']) $files3[] = $file;
+        }
+
+        return $files3;
+    }
+
     protected function uploadFile(?array $file): string
     {
         if ($file === null) {
