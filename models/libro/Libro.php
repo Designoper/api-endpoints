2095a9054d73479c92a01fed5223736f459ea268
a <a>
2025-03-04 14:22:36 +0100

    a

--- models/libro/Libro.php
+++ models/libro/Libro.php
@@ -17,20 +17,20 @@ final class Libro extends FileManager
 		$default = $this->getDefaultImage();
 		$statement =
 			"SELECT
-			libros.id_libro,
-			libros.titulo,
-			CASE
-				WHEN libros.portada IS NULL THEN '$default'
-				ELSE CONCAT('$host', libros.portada)
-			END AS portada,
-			libros.descripcion,
-			libros.paginas,
-			libros.fecha_publicacion,
-			DATE_FORMAT(libros.fecha_publicacion, '%d-%m-%Y') AS fecha_publicacion_dd_mm_yyyy,
-			categorias.categoria
-		FROM libros
-		NATURAL JOIN categorias
-		ORDER BY libros.titulo";
+				libros.id_libro,
+				libros.titulo,
+				CASE
+					WHEN libros.portada IS NULL THEN '$default'
+					ELSE CONCAT('$host', libros.portada)
+				END AS portada,
+				libros.descripcion,
+				libros.paginas,
+				libros.fecha_publicacion,
+				DATE_FORMAT(libros.fecha_publicacion, '%d-%m-%Y') AS fecha_publicacion_dd_mm_yyyy,
+				categorias.categoria
+			FROM libros
+			NATURAL JOIN categorias
+			ORDER BY libros.titulo";
 
 
 		$query = $this->getConnection()->prepare($statement);
