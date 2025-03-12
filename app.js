51c5fec62e8813770dc19ca6c2f9f9ac530281b4
a <a>
2025-03-12 20:29:30 +0100

    a

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+const serviceWorkerFile = './service-worker.js';
+
 if ('serviceWorker' in navigator) {
 	window.addEventListener('load', () => {
-		navigator.serviceWorker.register('./service-worker.js').then(registration => {
+		navigator.serviceWorker.register(serviceWorkerFile).then(registration => {
 			console.log('Service Worker registered with scope:', registration.scope);
 		}).catch(error => {
 			console.error('Service Worker registration failed:', error);
